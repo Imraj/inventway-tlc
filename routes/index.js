c3783cc63f536beb725d198f655ac6d275382537
Mujahid Raji <imraj3@live.co.uk>
2016-10-28 20:43:29 +0100

    latest commit

--- routes/index.js
+++ routes/index.js
@@ -200,8 +200,6 @@ router.get("/cars",function(req,res,next){
     res.redirect('login');
   }
 
-
-
 });
 
 router.get("/partners",function(req,res,next){
@@ -210,8 +208,7 @@ router.get("/partners",function(req,res,next){
   {
     Partner.find({},function(err,partner){
       if(err)return next(err);
-
-      res.render('cars',{title:'All partners',partners : partners,session:req.session });
+      res.render('partners',{title:'All partners',partners : partners,session:req.session });
     });
   }
   else{
