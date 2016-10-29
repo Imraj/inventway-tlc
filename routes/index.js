c52880fac75aad6f4cd82151ff1bc0649d588fda
Mujahid Raji <imraj3@live.co.uk>
2016-10-29 13:30:45 +0100

    latest

--- routes/index.js
+++ routes/index.js
@@ -270,7 +270,7 @@ router.get("/partner/:partner",function(req,res,next){
 });
 
 
-router.get('/inbox',function(req,res,next){
+router.get('/mail',function(req,res,next){
   if(req.session.valid){
       res.render('all_contacts',{title:'Inbox',session:req.session});
   }
