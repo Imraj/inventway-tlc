daefb0f9a85d8af23b836bcb52a7ea84b7e36065
Mujahid Raji <imraj3@live.co.uk>
2016-11-03 19:45:21 +0100

    la

--- public/js/app.js
+++ public/js/app.js
@@ -3,22 +3,3 @@ var app = angular.module("flapper",["ui.router"]);
 app.controller("HomeCtrl",["$scope","$state",function($scope,$state){
 
 }]);
-
-app.config({"$stateProvider","$urlRouterProvider",function($stateProvider,$urlRouterProvider){
-
-  $stateProvider
-    .state("home",{
-      url:"/home",
-      templateUrl:"/home.html",
-      controller:"HomeCtrl"
-    })
-
-    .state("about",{
-      url:"/about",
-      templateUrl:"/about.html",
-      controller:"HomeCtrl"
-    });
-
-  $urlRouterProvider.otherwise("home");
-
-}})
