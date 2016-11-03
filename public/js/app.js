52d91c9de327c091cfc3bad0e8d10e68ce0449e2
Mujahid Raji <imraj3@live.co.uk>
2016-11-03 20:47:05 +0100

    la

--- public/js/app.js
+++ public/js/app.js
@@ -7,5 +7,19 @@ apptlc.controller("HomeCtrl",["$scope","$state",function($scope,$state){
 apptlc.config([ "$stateProvider","$urlRouterProvider",
   function($stateProvider,$urlRouterProvider){
 
-}
-]);
+    $stateProvider
+      .state("home",{
+        templateUrl:"/home.html",
+        url:"/home",
+        controller:"HomeCtrl"
+      })
+
+      .state("about",{
+        templateUrl:"/about.html",
+        url:"/about",
+        controller:"HomeCtrl"
+      });
+
+    $urlRouterProvider.otherwise("home");  
+
+}]);
