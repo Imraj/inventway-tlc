267a182538bf5aa0fa823f8a623f30e18ea3cac5
Mujahid Raji <imraj3@live.co.uk>
2016-11-03 21:10:58 +0100

    la

--- app.js
+++ app.js
@@ -42,7 +42,10 @@ app.use(passport.session());
 app.use(flash());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+app.use(function(req, res) {
+    // Use res.sendfile, as it streams instead of reading the file into memory.
+    res.sendfile(__dirname + '/public/index.html');
+});
 
 app.use('/', routes);
 
