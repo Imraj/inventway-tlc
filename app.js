dc58e419db2d5dc00876fae6222176a9d44d7f61
Mujahid Raji <imraj3@live.co.uk>
2016-11-03 22:32:47 +0100

    la

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(flash());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/partials/:view',routes);
+//app.use('/partials/:view',routes);
 
 app.use('/users', users);
 
