d06cc885eaaa892f0201c351502d564cd9042f31
Mujahid Raji <imraj3@live.co.uk>
2016-11-04 18:29:37 +0100

    la

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,42 @@
 var apptlc = angular.module("flapper",["ui.router"]);
 
-apptlc.controller("HomeCtrl",["$scope","$state",function($scope,$state){
+apptlc.controller("HomeCtrl",["$scope","$state","RegistrationDataShareFactory",function($scope,$state,RegistrationDataShareFactory){
 
   $scope.categoriesA = ["Garage Dispatcher","Medallion Owner","Hotel Doorman","Garage Owner","Building Doorman",
                         "TLC Commisioner","Medallion Brookers or Corp.","DMV & TLC Lawyer","Radio Dispatcher",
                       "DMV Lawyer","City Dispatcher","TLC Lawyer"];
 
-  $scope.testd = "testd";
+  $scope.categoriesB1 = ["Already Experienced Driver","New Driver Currently got licensed by TLC",
+                        "New Student seeking to get  TLC License","Driver-Owned Vehicle(DOV)",
+                         "Individual-Owned Operator(Own Vehicle & Medallion)"];
 
-  $scope.user = {};
+  $scope.categoriesB2 = ["Yellow Cab","Gypsy & Radio","App Uber & Others","Green Cab","SUV",
+                         "Dial7 & Others","Black Car","Limousine","Commuter Van"];
+
+  $scope.categoriesC = ["Insurance company"]
+
+  $scope.testd = "atestd";
+
+  var rdata = RegistrationDataShareFactory.retrieveData();
+  console.log("rdata : " + rdata["email "] | rdata["password"]);
+  $scope.user.email = rdata["email"];
+  $scope.user.password = rdata["password"];
+
+  $scope.user = {
+    email: $scope.user.email | "",
+    password : $scope.user.password | "",
+    actor : $scope.user.actor | "",
+    driver_type : $scope.user.driver_type | "",
+    driver_community : $scope.user.driver_community | "",
+    business_type : $scope.user.business_type | ""
+  };
 
   $scope.uploadImage = function(){
 
   }
 
   $scope.saveUser = function(){
+    RegistrationDataShareFactory.saveData($scope.user.email,$scope.user.password);
     $state.go("complete_registeration");
   }
 
@@ -22,6 +44,23 @@ apptlc.controller("HomeCtrl",["$scope","$state",function($scope,$state){
       console.log("register button clicked");
       console.log(JSON.stringify($scope.user,null,4));
   }
+
+}]);
+
+apptlc.factory("RegistrationDataShareFactory",["$state","$scope",function($state,$scope){
+
+  var rdsf = {
+    var savedData = {};
+    saveData : function(email,pwd){
+      savedData["email"] = email;
+      savedData["pwd"] = pwd;
+    };
+    retrieveData : function(){
+      return savedData;
+    };
+  };
+
+  return rdsf;
 
 }]);
 
