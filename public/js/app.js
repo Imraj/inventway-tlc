d146a87842494fe07102c0678744986e66a5ada0
Mujahid Raji <imraj3@live.co.uk>
2016-11-05 02:10:02 +0100

    latest

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,7 @@
 var apptlc = angular.module("flapper",["ui.router","ngMessages","angularFileUpload"]);
 
-apptlc.controller("HomeCtrl",["$scope","$state","$rootScope","AuthFactory",function($scope,$state,$rootScope,AuthFactory){
+apptlc.controller("HomeCtrl",["$scope","$state","$rootScope","AuthFactory","FileUploader",
+                          function($scope,$state,$rootScope,AuthFactory,FileUploader){
 
   $scope.categoriesA = ["Garage Dispatcher","Medallion Owner","Hotel Doorman","Garage Owner","Building Doorman",
                         "TLC Commisioner","Medallion Brookers or Corp.","DMV & TLC Lawyer","Radio Dispatcher",
@@ -23,17 +24,18 @@ apptlc.controller("HomeCtrl",["$scope","$state","$rootScope","AuthFactory",funct
     actor :"",
     driver_type :"",
     driver_community :"",
-    business_type :""
+    business_type :"",
+    uploader:""
   };
+
+  $scope.user.uploader = new FileUploader();
+  console.info("uploader : " + $scope.uploader);
 
   if($rootScope._email != "")$scope.user.email = $rootScope._email;
   if($rootScope._password != "")$scope.user.password = $rootScope._password;
 
   console.log( $scope.user.email + " | " + $scope.user.password);
 
-  $scope.uploadImage = function(){
-
-  }
 
   $scope.saveUser = function(){
     $rootScope._email = $scope.user.email;
