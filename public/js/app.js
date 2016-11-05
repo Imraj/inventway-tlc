1633bfeb46aca61088130ba837a13cc7dd530c85
Mujahid Raji <imraj3@live.co.uk>
2016-11-05 02:21:46 +0100

    latest

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 var apptlc = angular.module("flapper",["ui.router","ngMessages","angularFileUpload"]);
 
-apptlc.controller("HomeCtrl",["$scope","$state","$rootScope","AuthFactory","FileUploader",
-                          function($scope,$state,$rootScope,AuthFactory,FileUploader){
+apptlc.controller("HomeCtrl",["$scope","$state","$rootScope","AuthFactory",
+                          function($scope,$state,$rootScope,AuthFactory){
 
   $scope.categoriesA = ["Garage Dispatcher","Medallion Owner","Hotel Doorman","Garage Owner","Building Doorman",
                         "TLC Commisioner","Medallion Brookers or Corp.","DMV & TLC Lawyer","Radio Dispatcher",
@@ -27,9 +27,6 @@ apptlc.controller("HomeCtrl",["$scope","$state","$rootScope","AuthFactory","File
     business_type :"",
     uploader:""
   };
-
-  $scope.user.uploader = new FileUploader();
-  console.info("uploader : " + $scope.uploader);
 
   if($rootScope._email != "")$scope.user.email = $rootScope._email;
   if($rootScope._password != "")$scope.user.password = $rootScope._password;
