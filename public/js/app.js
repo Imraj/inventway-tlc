59776cf3f52d1ced5e25de1508c7120187806c61
Mujahid Raji <imraj3@live.co.uk>
2016-11-07 01:43:08 +0100

    lal

--- public/js/app.js
+++ public/js/app.js
@@ -339,6 +339,7 @@ apptlc.controller("BaseAdsCtrl",["$scope","$state","$rootScope","filepickerServi
     $scope.saveAndExitAds = function(){
       AdFactory.saveAndExitAds($scope.ads)
                .success(function(data,status){
+                  console.log(JSON.stringify($scope.ads,null,4) + " | " + JSON.stringify(data,null,4));
                   if(data.success == true)
                   {
 
@@ -353,6 +354,7 @@ apptlc.controller("BaseAdsCtrl",["$scope","$state","$rootScope","filepickerServi
     $scope.payAndSubmitAds = function(){
       AdFactory.payAndSubmitAds($scope.ads)
                .success(function(data,status){
+                 console.log(JSON.stringify($scope.ads,null,4) + " | " + JSON.stringify(data,null,4));
                  if(data.success == true)
                  {
 
