fee65d48c785f5b68e5ea85d520e8a021dfac87e
Mujahid Raji <imraj3@live.co.uk>
2016-11-10 23:43:05 +0100

    latest commit

--- public/js/app.js
+++ public/js/app.js
@@ -129,6 +129,21 @@ apptlc.controller("BaseBuildHCtrl",["$scope","$state","$rootScope",function($sco
 
   $scope.eventSources = [];
 
+  $scope.uiConfig = {
+      calendar:{
+        height: 450,
+        editable: true,
+        header:{
+          left: 'month basicWeek basicDay agendaWeek agendaDay',
+          center: 'title',
+          right: 'today prev,next'
+        },
+        eventClick: $scope.alertEventOnClick,
+        eventDrop: $scope.alertOnDrop,
+        eventResize: $scope.alertOnResize
+      }
+    };
+
 }]);
 
 apptlc.controller("UnionCtrl",["$scope","$state","$rootScope","UnionFactory",function($scope,$state,$rootScope,UnionFactory){
