7443bede930c06cf55dbce558b53b1c07942d857
Mujahid Raji <imraj3@live.co.uk>
2016-11-10 13:44:38 +0100

    lal

---
+++ models/COD.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var CODSchema = mongoose.Schema({
+
+  article:String,
+  createdBy : String,
+  createdAt:{type:Date,default:Date.now}
+
+});
+
+CODSchema.pre("save",function(next){
+
+  now = new Date();
+  if(!this.createdAt){
+    this.createdAt = now;
+  }
+
+  return next();
+
+});
+
+mongoose.model("COD",CODSchema);
