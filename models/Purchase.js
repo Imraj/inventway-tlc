38363f71a2311bfae60173e204a2d922ad4a2931
Mujahid Raji <imraj3@live.co.uk>
2016-11-12 07:18:18 +0100

    la

---
+++ models/Purchase.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var PurchaseSchema = mongoose.Schema({
+
+    
+    createdBy:String,
+    createdAt:{type:Date,default:Date.now}
+
+});
+
+PurchaseSchema.pre("save",function(next){
+
+  now = new Date();
+  if(!this.createdAt){
+    this.createdAt = now;
+  }
+
+  return next();
+
+});
+
+mongoose.model("Purchase",PurchaseSchema);
